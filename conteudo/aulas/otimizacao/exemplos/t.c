a52ac9911d0ed8be3a74db3d514765210b17e560
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2019-06-17 12:29:15 -0300

    use two spaces

--- conteudo/aulas/otimizacao/exemplos/t.c
+++ conteudo/aulas/otimizacao/exemplos/t.c
@@ -2,8 +2,8 @@
 #include <sys/time.h>
 double gettime ()
 {
-        struct timeval tr;
-        gettimeofday(&tr, NULL);
-        return (double)tr.tv_sec+(double)tr.tv_usec/1000000;
+  struct timeval tr;
+  gettimeofday(&tr, NULL);
+  return (double)tr.tv_sec+(double)tr.tv_usec/1000000;
 }
 
